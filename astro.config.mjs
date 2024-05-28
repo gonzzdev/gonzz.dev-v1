acb30a09f663e9654de9bdaa5f3e0d788fdd6aa8
gonzz <streamofficemx@gmail.com>
2024-05-28 01:05:59 -0600

    Final styling and dark mode implementation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://astronaut.github.io',
-  base: 'my-repo'
+  site: 'https://gonzzdev.github.io',
+  base: ''
 });
